9811815fcb9690d1e3134033657645c258afeda5
VitaliyMaznyi <vitalii.maznyi@fasterthanlight.me>
2019-12-17 14:34:43 +0200

    refactor navbar

--- src/ui-kit/Navbar/Navbar.jsx
+++ src/ui-kit/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { useState } from 'react'
 import PT from 'prop-types'
 import {
   Button,
@@ -8,47 +8,40 @@ import {
 } from "@blueprintjs/core";
 import classes from './Navbar.scss'
 
-export default class Navbar extends PureComponent {
-  static propTypes = {
-    withSearch: PT.bool,
-  }
+const Navbar = ({ withSearch }) => {
+  const [searchValue, setSearchValue] = useState('');
 
-  static defaultProps = {
-    withSearch: false,
-  }
-
-  state = {
-    searchValue: ''
-  }
-
-  handleSearch = (searchValue) => {
-    this.setState({ searchValue })
-  }
-
-  render = () => {
-    const { withSearch } = this.props
-    return (
-        <BlueprintNavbar className={classes.navbar}>
-          <BlueprintNavbar.Group>
-            {withSearch && (
-              <InputGroup
-                  leftIcon="search"
-                  onChange={({ target }) => this.handleSearch(target.value)}
-                  placeholder="Search..."
-                  // rightElement={maybeSpinner}
-                  value={this.state.searchValue}
-              />
-            )}
-          </BlueprintNavbar.Group>
-          <BlueprintNavbar.Group className={classes.heading}>
-            <BlueprintNavbar.Heading>IMAGO</BlueprintNavbar.Heading>
-          </BlueprintNavbar.Group>
-          <BlueprintNavbar.Group className={classes.actions} align={Alignment.RIGHT}>
-            <p>Hello, stranger!</p>
-            <BlueprintNavbar.Divider />
-            <Button className="bp3-minimal" icon="plus" text="Add image" />
-          </BlueprintNavbar.Group>
-        </BlueprintNavbar>
-    )
-  }
+  return (
+    <BlueprintNavbar className={classes.navbar}>
+      <BlueprintNavbar.Group>
+        {withSearch && (
+            <InputGroup
+                leftIcon="search"
+                onChange={({ target }) => setSearchValue(target.value)}
+                placeholder="Search..."
+                // rightElement={maybeSpinner}
+                value={searchValue}
+            />
+        )}
+      </BlueprintNavbar.Group>
+      <BlueprintNavbar.Group className={classes.heading}>
+        <BlueprintNavbar.Heading>IMAGO</BlueprintNavbar.Heading>
+      </BlueprintNavbar.Group>
+      <BlueprintNavbar.Group className={classes.actions} align={Alignment.RIGHT}>
+        <p>Hello, stranger!</p>
+        <BlueprintNavbar.Divider />
+        <Button className="bp3-minimal" icon="plus" text="Add image" />
+      </BlueprintNavbar.Group>
+    </BlueprintNavbar>
+  )
 }
+
+Navbar.propTypes = {
+  withSearch: PT.bool,
+}
+
+Navbar.defaultProps = {
+  withSearch: false,
+}
+
+export default Navbar
