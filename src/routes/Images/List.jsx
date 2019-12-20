4def650082f48f9bf072e87c6af10192fcce1a76
VitaliyMaznyi <vitalii.maznyi@fasterthanlight.me>
2019-12-20 19:01:18 +0200

    add edit button to ImageCard

--- src/routes/Images/List.jsx
+++ src/routes/Images/List.jsx
@@ -66,6 +66,7 @@ const List = ({ searchValue, saveImageIds, history, applySearch }) => {
         {images.map(image => (
           <ImageCard
             key={image.id}
+            imageId={image.id}
             imageURL={image.url.small || image.url.raw}
             title={`@${image.author.username}`}
             subtitle={image.description}
--- src/ui-kit/ImageCard/ImageCard.jsx
+++ src/ui-kit/ImageCard/ImageCard.jsx
@@ -1,16 +1,18 @@
 import React, { PureComponent } from 'react'
 import PT from 'prop-types'
-import { Card, Elevation } from "@blueprintjs/core";
+import { withRouter } from 'react-router-dom'
+import { Card, Elevation, Icon } from "@blueprintjs/core";
 import cx from 'classnames'
 import classes from './ImageCard.scss'
 
-export default class ImageCard extends PureComponent {
+class ImageCard extends PureComponent {
   static propTypes = {
     imageURL: PT.string.isRequired,
     title: PT.string.isRequired,
     subtitle: PT.string,
     classNames: PT.string,
-    handleClick: PT.func.isRequired
+    handleClick: PT.func.isRequired,
+    imageId: PT.string.isRequired
   }
 
   static defaultProps = {
@@ -19,15 +21,26 @@ export default class ImageCard extends PureComponent {
   }
 
   render = () => {
-    const { imageURL, title, subtitle, classNames, handleClick } = this.props
+    const { imageURL, title, subtitle, classNames, handleClick, imageId, history } = this.props
     const cardClasses = cx(classNames, classes.imageCard)
 
     return (
-      <Card className={cardClasses} interactive elevation={Elevation.TWO} onClick={handleClick}>
-        <div className={classes.title}>{title}</div>
+      <Card className={cardClasses} interactive elevation={Elevation.TWO}>
+        <div className={classes.titleRow}>
+          <div className={classes.title}>{title}</div>
+          <Icon
+            icon="edit"
+            className={classes.editIcon}
+            iconSize={20}
+            interactive={'false'}
+            onClick={() => history.push(`/images/${imageId}/edit`)}
+          />
+        </div>
         <div className={classes.subtitle}>{subtitle}</div>
-        <img key={imageURL} src={imageURL} alt={imageURL} />
+        <img key={imageURL} src={imageURL} alt={imageURL} onClick={handleClick} />
       </Card>
     )
   }
 }
+
+export default withRouter(ImageCard)
